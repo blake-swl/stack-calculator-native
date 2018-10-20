4e455ed61ae26fa40ec4ebe23e949322aad9bcca
blake-swl <seulwoolee93@gmail.com>
2018-10-20 16:11:37 -0700

    [add] - button

--- stack_calculator/App.js
+++ stack_calculator/App.js
@@ -1,21 +1,52 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import Button from './Button';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  top: {
+    paddingTop: 20,
+    backgroundColor: 'blue',
+  },
+  bottom: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  number: {
+    color: 'white',
+    padding: 10,
+    fontSize: 30,
+    fontWeight: 'bold',
+    backgroundColor: '#424242',
+    textAlign: 'right',
+    borderBottomWidth: 1,
+    borderColor: 'white'
+  },
+  row: {
+
+  }
 });
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <Text style={styles.number}>0</Text>
+        <Text style={styles.number}>0</Text>
+        <Text style={styles.number}>0</Text>
+      </View>
+      <View style={styles.bottom}>
+        <View style={styles.row}>
+          <Button text="clear" />
+        </View>
+      </View>
+    </View>
+  );
+};
+export default App;
---
+++ stack_calculator/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const Button = ({text}) => (
+  <Text>{text}</Text>
+);
+
+export default Button;
