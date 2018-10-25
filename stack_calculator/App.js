aaaaa6de4a434b636defe0a7993b0ccfd6e7b806
blake-swl <seulwoolee93@gmail.com>
2018-10-25 00:58:46 -0700

    [edit] - layout

--- stack_calculator/App.js
+++ stack_calculator/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Button from './Button';
+import { white } from 'ansi-colors';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,24 +13,27 @@ const styles = StyleSheet.create({
   },
   top: {
     paddingTop: 20,
-    backgroundColor: 'blue',
+    backgroundColor: '#2E5060',
   },
   bottom: {
     flex: 1,
-    backgroundColor: 'red',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black'
+    // backgroundColor: 'red',
   },
   number: {
     color: 'white',
     padding: 10,
     fontSize: 30,
     fontWeight: 'bold',
-    backgroundColor: '#424242',
+    backgroundColor: '#2E5060',
     textAlign: 'right',
-    borderBottomWidth: 1,
-    borderColor: 'white'
   },
   row: {
-
+    flex: 1,
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    borderBottomColor: 'black',
   }
 });
 
@@ -44,6 +48,32 @@ const App = () => {
       <View style={styles.bottom}>
         <View style={styles.row}>
           <Button text="clear" />
+          <Button text="pow" />
+          <Button text="swap" />
+          <Button text="/" />
+        </View>
+        <View style={styles.row}>
+          <Button text="9" />
+          <Button text="8" />
+          <Button text="7" />
+          <Button text="x" />
+        </View>
+        <View style={styles.row}>
+          <Button text="6" />
+          <Button text="5" />
+          <Button text="4" />
+          <Button text="-" />
+        </View>
+        <View style={styles.row}>
+          <Button text="3" />
+          <Button text="2" />
+          <Button text="1" />
+          <Button text="+" />
+        </View>
+        <View style={styles.row}>
+          <Button text="0" />
+          <Button text="." />
+          <Button text="enter" />
         </View>
       </View>
     </View>
--- stack_calculator/Button.js
+++ stack_calculator/Button.js
@@ -1,8 +1,23 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    borderColor: 'black'
+  },
+  text: {
+    fontSize: 36,
+  }
+})
 
 const Button = ({text}) => (
-  <Text>{text}</Text>
+  <View style={styles.container}>
+    <Text style={styles.text}>{text}</Text>
+  </View>
 );
 
 export default Button;
