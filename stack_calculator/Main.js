07c6980992ec2ef6433e5a33d92103627f2c69d7
blake-swl <seulwoolee93@gmail.com>
2018-10-26 21:54:54 -0700

    [add] - animations

--- stack_calculator/Main.js
+++ stack_calculator/Main.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import * as Animatable from 'react-native-animatable';
 
-import { pressNum, enter, operation } from './modules';
+import { pressNum, enter, operation, clear, swap, toggleNegative } from './modules';
 import Button from './Button';
 
 const baseNumber = {
@@ -11,16 +12,18 @@ const baseNumber = {
   textAlign: 'right',
   padding: 10,
   fontSize: 40,
-  borderBottomWidth: 1,
-  borderColor: '#fff',
 };
 
 const styles = StyleSheet.create({
+  bottomBorder: {
+    borderBottomWidth: 1,
+    borderColor: '#fff',
+  },
   container: {
     flex: 1,
   },
   top: {
-    paddingTop: 20,
+    paddingTop: Platform.OS === 'ios' ? 32 : 20,
   },
   bottom: {
     flex: 1,
@@ -45,51 +48,85 @@ const styles = StyleSheet.create({
   },
 });
 
-const App = ({
-  calculatorState: { stack, inputState },
-  operationAction,
-  pressNumWithDispatch,
-  enterAction,
-}) => (
-  <View style={styles.container}>
-    <View style={styles.top}>
-      <Text style={styles.append}>{stack[2] || 0}</Text>
-      <Text style={styles.append}>{stack[1] || 0}</Text>
-      <Text style={styles[inputState]}>{stack[0] || 0}</Text>
-    </View>
-    <View style={styles.bottom}>
-      <View style={styles.row}>
-        <Button text="clear" />
-        <Button text="pow" onPress={operationAction} />
-        <Button text="swap" />
-        <Button text="/" onPress={operationAction} />
+class App extends React.Component {
+  render() {
+    const {
+      calculatorState: { stack, inputState },
+      operationAction,
+      pressNumWithDispatch,
+      enterAction,
+      clearAction,
+      swapAction,
+      toggleNegativeAction,
+    } = this.props;
+    return (
+      <View style={styles.container}>
+        <View style={styles.top}>
+          <TouchableOpacity style={styles.bottomBorder} onPress={() => toggleNegativeAction(2)}>
+            <Text style={styles.append}>{stack[2] || 0}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.bottomBorder} onPress={() => toggleNegativeAction(1)}>
+            <Animatable.Text
+              ref={(ref) => {
+                this.text2 = ref;
+              }}
+              style={styles.append}
+            >
+              {stack[1] || 0}
+            </Animatable.Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => toggleNegativeAction(0)}>
+            <Animatable.Text
+              ref={(ref) => {
+                this.text1 = ref;
+              }}
+              style={styles[inputState]}
+            >
+              {stack[0] || 0}
+            </Animatable.Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.bottom}>
+          <View style={styles.row}>
+            <Button text="clear" onPress={clearAction} />
+            <Button text="pow" onPress={operationAction} />
+            <Button text="swap" onPress={swapAction} />
+            <Button text="/" onPress={operationAction} />
+          </View>
+          <View style={styles.row}>
+            <Button text="9" onPress={pressNumWithDispatch} />
+            <Button text="8" onPress={pressNumWithDispatch} />
+            <Button text="7" onPress={pressNumWithDispatch} />
+            <Button text="X" onPress={operationAction} />
+          </View>
+          <View style={styles.row}>
+            <Button text="6" onPress={pressNumWithDispatch} />
+            <Button text="5" onPress={pressNumWithDispatch} />
+            <Button text="4" onPress={pressNumWithDispatch} />
+            <Button text="-" onPress={operationAction} />
+          </View>
+          <View style={styles.row}>
+            <Button text="3" onPress={pressNumWithDispatch} />
+            <Button text="2" onPress={pressNumWithDispatch} />
+            <Button text="1" onPress={pressNumWithDispatch} />
+            <Button
+              text="+"
+              onPress={(x) => {
+                operationAction(x);
+                this.text1.flash(500);
+              }}
+            />
+          </View>
+          <View style={styles.row}>
+            <Button text="0" onPress={pressNumWithDispatch} />
+            <Button text="." onPress={pressNumWithDispatch} />
+            <Button text="enter" onPress={enterAction} special />
+          </View>
+        </View>
       </View>
-      <View style={styles.row}>
-        <Button text="9" onPress={pressNumWithDispatch} />
-        <Button text="8" onPress={pressNumWithDispatch} />
-        <Button text="7" onPress={pressNumWithDispatch} />
-        <Button text="X" onPress={operationAction} />
-      </View>
-      <View style={styles.row}>
-        <Button text="6" onPress={pressNumWithDispatch} />
-        <Button text="5" onPress={pressNumWithDispatch} />
-        <Button text="4" onPress={pressNumWithDispatch} />
-        <Button text="-" onPress={operationAction} />
-      </View>
-      <View style={styles.row}>
-        <Button text="3" onPress={pressNumWithDispatch} />
-        <Button text="2" onPress={pressNumWithDispatch} />
-        <Button text="1" onPress={pressNumWithDispatch} />
-        <Button text="+" onPress={operationAction} />
-      </View>
-      <View style={styles.row}>
-        <Button text="0" onPress={pressNumWithDispatch} />
-        <Button text="." onPress={pressNumWithDispatch} />
-        <Button text="enter" onPress={enterAction} special />
-      </View>
-    </View>
-  </View>
-);
+    );
+  }
+}
 
 export default connect(
   state => ({ calculatorState: state }),
@@ -99,6 +136,9 @@ export default connect(
         pressNumWithDispatch: pressNum,
         enterAction: enter,
         operationAction: operation,
+        clearAction: clear,
+        swapAction: swap,
+        toggleNegativeAction: toggleNegative,
       },
       dispatch,
     ),
